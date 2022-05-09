ae09b19b9b3ec88facd89ba0346c205fe17866d2
Carlos Carral C <carloscarral13@gmail.com>
2022-05-09 10:57:09 -0500

    Añade `vite-plugin-wasm-pack` como dependencia.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import wasmPack from 'vite-plugin-wasm-pack'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  serve:{
-    host: '0.0.0.0'
-  }
+    // El primer Array que se pasa a wasmPack es para
+    // módulos en una ruta en el sistema de archivos local y el segundo
+    // para módulos instalados con npm.
+    plugins: [vue(), wasmPack([], ['uaemex-horarios'])],
+    serve: {
+        host: '0.0.0.0'
+    },
 })
