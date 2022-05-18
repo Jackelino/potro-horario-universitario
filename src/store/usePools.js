395a1ccbb8726299af7be3e00743b54e8f50417a
Carlos Carral C <carloscarral13@gmail.com>
2022-05-18 15:57:52 -0500

    Elimina la obtencion de materias del engine.
    A su vez, las materias se convierten en una propiedad calculada
    dinámicamente del store y de los objetos pool.

--- src/store/usePools.js
+++ src/store/usePools.js
@@ -1,18 +1,23 @@
 import { defineStore } from 'pinia';
+import { EngineParams } from '../lib/engineParams';
 
 export const usePoolStore = defineStore('pools', {
     state: () => ({
         pools: [],
-        subjects: []
+        engineParams: new EngineParams()
     }),
     actions: {
         addToSubjects(subjects) {
             // Verificar que no tenemos dos materias con el mismo id
-            for (const newSubject of subjects) {
-                let idList = newSubject.subject_id.id_list;
-                for (const sub of this.subjects) {
+
+            Array.prototype.push.apply(this.subjects, subjects);
+        },
+        addToPools(pools) {
+            for (const newPool of subjects) {
+                let idList = newPool.pool_id.id_list;
+                for (const pool of this.pools) {
                     for (const newId of idList) {
-                        for (const id of sub.subject_id.id_list) {
+                        for (const id of pool.pool_id.id_list) {
                             if (newId === id) {
                                 throw new Error(`Materias con id repetidos: ${id}`);
                             }
@@ -20,10 +25,19 @@ export const usePoolStore = defineStore('pools', {
                     }
                 }
             }
-            Array.prototype.push.apply(this.subjects, subjects);
-        },
-        addToPools(pools) {
             Array.prototype.push.apply(this.pools, pools);
+        },
+        addPoolToEngineParams(pool) {
+            // Buscar pool en this.pools y eliminarlo.
+            let poolIdx = this.pools.indexOf(pool);
+            if (poolIdx !== -1) {
+                this.pools.splice(poolIdx, 1);
+                this.engineParams.addPool(pool);
+            }
+        },
+        removePoolFromEngineParams(pool) {
+            this.engineParams.removePool(pool);
+            this.pools.push(pool);
         }
     },
     getters: {
@@ -36,6 +50,16 @@ export const usePoolStore = defineStore('pools', {
                 }
             }
             return groups;
+        },
+        subjects: (state) => {
+            return state.pools.map((val) => ({
+                name: val.grid_list[0].data.nombre,
+                subject_id: val.pool_id
+            }));
+
+        },
+        selectedSubjects: (state) => {
+            return state.engineParams.subjects;
         }
     }
 });
