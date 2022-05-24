8a18820f0b647dec95e40f776fdce198d662b85e
JoseFernandez2020 <codigo.gratuito2019@gmail.com>
2022-05-23 20:49:58 -0500

    se crea componente Paginate con su comportoamiento

---
+++ src/components/Pagination.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="paginate">
+    <nav aria-label="Page navigation example" class="p-1">
+      <ul class="pagination justify-content-end">
+        <li class="page-item disabled">
+          <a class="page-link"><i class="fa-solid fa-angle-left"></i></a>
+        </li>
+        <li class="page-item" v-for="(item, index) in engineResults[0].grids" :key="index"><a class="page-link" href="#">{{index + 1}}</a></li>
+        <li class="page-item">
+          <a class="page-link" href="#"><i class="fa-solid fa-angle-right"></i></a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { mapState} from 'pinia';
+import { useEngineResults} from "../store/useEngineResults";
+export default {
+  name: "Pagination",
+  data() {
+    return {
+      numberGrids: 0
+    }
+  },
+  computed: {
+    ...mapState( useEngineResults, ['engineResults'])
+  },
+
+}
+</script>
+
+<style>
+
+</style>
