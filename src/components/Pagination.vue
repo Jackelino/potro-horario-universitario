bcd8e746647a89642cbd759ba8d5f0c8dd86e803
Carlos Carral C <carloscarral13@gmail.com>
2022-06-01 19:32:55 -0500

    Estilo condicional de página seleccionada

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -3,9 +3,12 @@
     <nav aria-label="Page navigation example" class="p-1">
       <ul class="pagination justify-content-end">
         <li class="page-item">
-          <a @click="decCurrentResultIdx" class="page-link"><i class="fa-solid fa-angle-left"></i></a>
+          <a @click="decCurrentResultIdx" class="page-link" href="#"><i class="fa-solid fa-angle-left"></i></a>
         </li>
-        <li class="page-item" v-for="index in resultsArrayLen" :key="index"><a class="page-link" href="#">{{index}}</a></li>
+        <li class="page-item" v-for="index in resultsArrayLen"
+        :key="index"><a class="page-link" href="#"
+        :class="{'number-highlight': index == currentResultIdx + 1}">{{index}}</a></li>
+        
         <li class="page-item">
           <a @click="incCurrentResultIdx" class="page-link" href="#"><i class="fa-solid fa-angle-right"></i></a>
         </li>
@@ -15,15 +18,24 @@
 </template>
 
 <script>
-import { mapActions } from "pinia";
+import { mapActions, mapState} from "pinia";
 import { useScheduleView } from "../store/useScheduleView";
 export default {
   name: "Pagination",
   props:{
     resultsArrayLen: Number 
   },
+  computed:{
+    ...mapState(useScheduleView, ["currentResultIdx"]),
+  },
   methods:{
     ...mapActions(useScheduleView, ["incCurrentResultIdx", "decCurrentResultIdx"]),
   }
 }
 </script>
+<style scoped>
+.number-highlight{
+    font-weight:900;
+    background-color:#D2D5D9;
+}
+</style>
